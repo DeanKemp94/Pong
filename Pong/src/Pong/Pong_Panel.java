e66dd15201072069940dcadd25e28c748473a315
DeanKemp94 <120429231+DeanKemp94@users.noreply.github.com>
2024-12-05 11:23:16 +1300

    Update 1.0
    Timmer object added to game logic class
    paint component method added to allow panel to have a background

--- Pong/src/Pong/Pong_Panel.java
+++ Pong/src/Pong/Pong_Panel.java
@@ -6,13 +6,30 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class Pong_Panel extends JPanel implements ActionListener, KeyListener {
     private final static Color BACKGROUND_COLOUR = Color.BLACK;
+    private final static int TIMER_DELAY = 5;
 
     public Pong_Panel() {
         setBackground(BACKGROUND_COLOUR);
+        Timer timer = new Timer(TIMER_DELAY, this);
+            timer.start();
     }
+
+    void update(){
+
+    }
+
+     @Override
+     public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.WHITE);
+        g.fillRect(20, 20, 100, 100);
+         }
+
     @Override
     public void actionPerformed(ActionEvent event) {
 
