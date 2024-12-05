a3ae67151467e3ab58b3ad7619d9e8bf13124ed7
DeanKemp94 <120429231+DeanKemp94@users.noreply.github.com>
2024-12-05 13:53:09 +1300

    Update to Sprite Class
    Created get and set methods to get X & Y position.
    Created methods to allow the position of a Sprite to be updated based on the bounds of the panel.
    imported and create the retabgle ball.

--- Pong/src/Pong/Sprite.java
+++ Pong/src/Pong/Sprite.java
@@ -1,4 +1,99 @@
 package Pong;
 
-abstract class Sprite {
+import java.awt.*;
+
+class Sprite {
+    private int initialXPosition;
+    private int initialYPosition;
+    private int xPosition, yPosition;
+    private int xVelocity,yVelocity;
+    private int width, height;
+    private Color color;
+
+    public int getXPosition() {
+        return xPosition;
+    }
+
+    public void setXPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public int getYPosition() {
+        return yPosition;
+    }
+
+    public void setYPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    public int getXVelocity() {
+        return xVelocity;
+    }
+
+    public void setXVelocity(int xVelocity) {
+        this.xVelocity = xVelocity;
+    }
+
+    public int getYVelocity() {
+        return yVelocity;
+    }
+
+    public void setYVelocity(int yVelocity) {
+        this.yVelocity = yVelocity;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setXPosition(int newX, int panelWidth) {
+        xPosition = newX;
+        if (xPosition < 0) {
+            xPosition = 0;
+        } else if (xPosition + width > panelWidth) {
+            xPosition = panelWidth - width;
+        }
+    }
+     public void setYPosition(int newY, int panelWidth) {
+         yPosition = newY;
+         if (yPosition < 0) {
+             yPosition = 0;
+         } else if (yPosition + width < panelWidth) {
+             yPosition = panelWidth - width;
+         }
+     }
+
+     public void setInitialPosition(int initialX, int initialY){
+        initialXPosition = initialX;
+        initialYPosition = initialY;
+     }
+
+     public void resetToInitialPosition() {
+        setXPosition(initialXPosition);
+        setYPosition(initialYPosition);
+     }
+
+     public Rectangle getRectangle() {
+        return new Rectangle(getXPosition(), getYPosition(), getWidth(), getHeight());
+     }
 }
